e1f3995b903914a94615affcdf821e6b6614cbab
manuelalvarezsolt <manuel.alvarez.solt@gmail.com>
2026-03-17 11:14:01 -0300

    Update names.py

--- names.py
+++ names.py
@@ -1,5 +1,13 @@
+from http.cookiejar import uppercase_escaped_char
+
+
 def names():
     """Lee nombre y apellido, e imprime el nombre completo en distintos formatos:
     minúsculas, título, mayúsculas y con tabulador.
     """
     pass
+    nombre = input("cual es tu nombre")
+    print(f"{nombre.lower()}")
+    print(f"{nombre.upper()}")
+    print(f"{nombre.title()}")
+    print(f"\t{nombre}")
