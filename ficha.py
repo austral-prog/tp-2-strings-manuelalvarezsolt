9b5f76d113333bdc7990ff3f46effd3697f05f56
manuelalvarezsolt <manuel.alvarez.solt@gmail.com>
2026-03-23 20:51:09 -0300

    Update ficha.py

--- ficha.py
+++ ficha.py
@@ -25,3 +25,34 @@ def ficha():
     #   - Las 3 notas, su suma, promedio y promedio entero
     #   - Cierre decorativo usando repetición de string ("=" * 24)
     pass
+    nombre = input()
+    mail = input()
+    nota1 = int(input())
+    nota2 = int(input())
+    nota3 = int(input())
+    print('''========================
+    FICHA DEL ALUMNO
+========================''')
+    print(f"Nombre: {nombre.title().strip()}")
+    print(f"Email: {mail.lower()}")
+    print(f"Caracteres en nombre: {len(nombre)}")
+    espacio = nombre.find(" ")
+    print(f"Iniciales: {nombre[0].upper()}{nombre[espacio + 1].upper()}")
+    print(f"Usuario: {nombre[espacio + 1 : ].lower()}.{nombre[0 : espacio].lower()}")
+    print(f"Email valido: {"@" in mail}")
+    arroba = mail.find("@")
+    print(f"Dominio: {mail[arroba + 1 :]}")
+    nombre2 = nombre.replace(" ", "_")
+    print(f"Nombre para archivo {nombre2.title().strip()}")
+    print(f"Cantidad de a: {nombre.lower().count('a')}")
+    print(f"Codigo secreto: {nombre[::-1].upper()}")
+    sumanotas = nota1 + nota2 + nota3
+    promedio = sumanotas / 3
+    promedioent = sumanotas // 3
+    print(f"Nota 1: {nota1}")
+    print(f"Nota 1: {nota2}")
+    print(f"Nota 1: {nota3}")
+    print(f"Suma: {sumanotas}")
+    print(f"Promedio: {promedio}")
+    print(f"Promedio entero: {int(promedioent)}")
+    print("=" * 24)
